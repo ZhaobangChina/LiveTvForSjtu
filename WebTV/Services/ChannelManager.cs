513b0d0605d774ad9b9f002aa18414736f63f92e
Huang, Zhaoquan <zhaoquan2008@hotmail.com>
2018-04-10 10:31:04 +0800

    添加项目文件。

---
+++ WebTV/Services/ChannelManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace WebTV.Services
+{
+    static class ChannelManager
+    {
+        /// <summary>
+        /// 从网络上获取频道列表
+        /// </summary>
+        /// <returns>获取到的频道列表</returns>
+        /// <exception cref="System.Net.Http.HttpRequestException">
+        /// 进行网络请求时发生错误。
+        /// </exception>
+        public static async Task<IEnumerable<Channel>> GetChannelsAsync()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var uri = new Uri("http://comic.sjtu.edu.cn/vlc/comic.xspf");
+                HttpResponseMessage response = await client.GetAsync(uri, HttpCompletionOption.ResponseContentRead);
+                response.EnsureSuccessStatusCode();
+
+                Stream xmlStream = await response.Content.ReadAsStreamAsync();
+                Xspf xspf = Xspf.Load(xmlStream);
+                return xspf.TrackList.Select(xspfTrack =>
+                    new Channel
+                    {
+                        Name = xspfTrack.Title,
+                        Url = xspfTrack.Location.ToString()
+                    });
+            }
+        }
+    }
+
+    class Channel
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ WebTV/ViewModels/MainPageVM.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+
+namespace WebTV.ViewModels
+{
+    class MainPageVM : INotifyPropertyChanged
+    {
+        public MainPageVM(CoreDispatcher dispatcher)
+        {
+            this.dispatcher = dispatcher;
+            var result = LoadChannelListAsync();
+        }
+
+        private CoreDispatcher dispatcher;
+
+        private bool _loading = true;
+
+        public bool Loading
+        {
+            get => _loading;
+            private set
+            {
+                if (_loading != value)
+                {
+                    _loading = value;
+                    NotifyPropertyChanged(nameof(Loading));
+                }
+            }
+        }
+
+        private ObservableCollection<Services.Channel> _channelList;
+
+        public ObservableCollection<Services.Channel> ChannelList
+        {
+            get => _channelList;
+            private set
+            {
+                if (_channelList != value)
+                {
+                    _channelList = value;
+                    NotifyPropertyChanged(nameof(ChannelList));
+                }
+            }
+        }
+
+        private Services.Channel _selectedChannel;
+
+        public Services.Channel SelectedChannel
+        {
+            get => _selectedChannel;
+            set
+            {
+                if (_selectedChannel != value)
+                {
+                    _selectedChannel = value;
+                    NotifyPropertyChanged(nameof(SelectedChannel));
+                    ChannelName = _selectedChannel?.Name;
+                    Url = _selectedChannel?.Url;
+                }
+            }
+        }
+
+        private string _channelName;
+
+        public string ChannelName
+        {
+            get => _channelName;
+            private set
+            {
+                if (_channelName != value)
+                {
+                    _channelName = value;
+                    NotifyPropertyChanged(nameof(ChannelName));
+                }
+            }
+        }
+
+        private string _url;
+
+        public string Url
+        {
+            get => _url;
+            private set
+            {
+                if (_url != value)
+                {
+                    _url = value;
+                    NotifyPropertyChanged(nameof(Url));
+                }
+            }
+        }
+
+        private void ShowError(Exception ex)
+        {
+            var dialog = new Windows.UI.Xaml.Controls.ContentDialog
+            {
+                Content = string.Format("出现错误：{0}", ex.Message),
+                CloseButtonText = "忽略"
+            };
+            var result = dialog.ShowAsync();
+        }
+
+        public void Refresh()
+        {
+            var result = LoadChannelListAsync();
+        }
+
+        private async Task LoadChannelListAsync()
+        {
+            Loading = true;
+            try
+            {
+                var channels = await Services.ChannelManager.GetChannelsAsync();
+                ChannelList = new ObservableCollection<Services.Channel>(channels);
+            }
+            catch (Exception ex)
+            {
+                ShowError(ex);
+            }
+            Loading = false;
+        }
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            void action()
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+            if (dispatcher.HasThreadAccess)
+                action();
+            else
+                dispatcher.RunAsync(CoreDispatcherPriority.Normal, action).AsTask().Wait();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
